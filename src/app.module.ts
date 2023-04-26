453aed8ead9579c1f2fb3a2e291ee37aee6b770b
Andrey Zuev <stepgt@mail.ru>
2023-04-26 14:07:40 +1000

    Added @nestjs/config && .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,18 @@ import { FilesModule } from './files/files.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users/entities/user.entity';
 import { FileEntity } from './files/entities/file.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'horton.db.elephantsql.com',
-      port: 5432,
-      username: 'hithewpp',
-      password: 'Geuyp6kuIqewSKqnlejYKP8nxZhWYmS7',
-      database: 'hithewpp',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [UserEntity, FileEntity],
       synchronize: true,
     }),
