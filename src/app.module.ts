7b1a643aec77ca62ef1ffe2f23adc0195ab960d4
Andrey Zuev <stepgt@mail.ru>
2023-04-26 10:28:26 +1000

    Added pg && typeorm && @nestjs/typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FilesModule } from './files/files.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, FilesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'horton.db.elephantsql.com',
+      port: 5432,
+      username: 'hithewpp',
+      password: 'Geuyp6kuIqewSKqnlejYKP8nxZhWYmS7',
+      database: 'hithewpp',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
