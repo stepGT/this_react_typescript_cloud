141e61ec314da96824e7700a68220cd16b03123d
Andrey Zuev <stepgt@mail.ru>
2023-04-28 09:50:17 +1000

    Added findByEmail && findById && create methods users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,21 +24,6 @@ export class UsersController {
 
   @Get()
   findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.findByEmail('stepgt@mail.ru');
   }
 }
