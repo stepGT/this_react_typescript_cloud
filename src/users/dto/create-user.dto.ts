d1b59542bff7ed87721063ddc31f0402af7a24d7
Andrey Zuev <stepgt@mail.ru>
2023-04-28 10:21:25 +1000

    Added resource Auth && Strategy

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    default: 'stepgt@mail.ru',
+  })
   email: string;
+
+  @ApiProperty({
+    default: 'stepGT',
+  })
   fullName: string;
+
+  @ApiProperty({
+    default: '777',
+  })
   password: string;
 }
